1d3e0bbb6c6134fa600a32b1d2ba0c8f8865119f
gabrielnicoleli <gabriel.nicoleli@primecontrol.com.br>
2019-03-17 10:58:49 -0300

    first controller

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles MVS Teste.";
+            ViewData["By"] = "gmnicoleli";
 
             return View();
         }
