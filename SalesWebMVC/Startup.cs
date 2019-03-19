c2fafb6d343b15ac78a87ea15f3a99f9378b982f
gabrielnicoleli <gabriel.nicoleli@primecontrol.com.br>
2019-03-19 09:49:15 -0300

    crud novo

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -37,8 +37,11 @@ namespace SalesWebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
+ //SQLSERVER         options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
         }
+
+        //incluir dependencias do mysql 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
